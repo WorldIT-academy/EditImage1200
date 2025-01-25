ffb9a361805110384d89932f1eba769994185ed7
WorldIT-academy <worldit_academy@worldit.academy>
2025-01-25 13:45:59 +0200

    create comment in the class AppButton and module app_button.py

--- modules/gui/app_button.py
+++ modules/gui/app_button.py
@@ -1,17 +1,45 @@
+r'''
+    У модулі розміщено інструкцію клас (AppButton), ціль якого створювати та налаштовувати кнопки 
+    у графічному інтерфейсі будь якого застосунку
+    
+    Модулі:
+        - `customtkinter` - для створення вікна кнопки
+        - `PIL` - для завантаження зображення з папки static/icons
+        - `os` - для роботи з папками та файлами
+    
+    Приклад застосування: 
+    >>> app_button = AppButton(ch_master=self, name_icon='edit.png')
+'''
 import os
 import customtkinter as ctk
 from PIL import Image
 
 class AppButton(ctk.CTkButton):
-    def __init__(self, ch_master: object, name_icon: str, **kwargs):
+    r"""
+        Конструктор (__init__) створює кнопку з завантаженим зображенням з папки static/icons
+        
+        parameter :mod:`ch_master` - у параметрі вказуємо до якого вікна (фрейму) додається кнопка;
+        parameter :mod:`name_icon` - у параметрі вказуємо назву зображення з папки static/icons;
+        parameter :mod:`size_side` - у параметрі вказуємо розмір сторони зображення в пікселях;
+        
+        Методи:
+            :mod:`load_image` - завантажує зображення з папки static/icons з заданим розміром;
+            :mod:`os.path.abspath` - для роботи з папками та файлами;
+        Класи:
+            :mod:`ctk.CTkButton` - для створення кнопки;
+            :mod:`PIL.Image.open` - відкриває зображення та повертає значення до класу CTkImage;
+            :mod:`ctk.CTkImage` - задає формат зображенню в CTkImage для відображення у графічному інтерфейсі застосунку
+        
+    """
+    def __init__(self, ch_master: object, name_icon: str, size_side: float, **kwargs):
         self.NAME_ICON = name_icon
-        self.MASTER = ch_master
+        self.SIZE = (int(size_side), int(size_side))
         ctk.CTkButton.__init__(
             self,
             master = ch_master,
             text= '',
-            width= self.MASTER._current_height * 0.7,
-            height= self.MASTER._current_height * 0.7,
+            width=  int(size_side),
+            height= int(size_side),
             image= self.load_image(),
             **kwargs
         )
@@ -19,6 +47,6 @@ class AppButton(ctk.CTkButton):
         PATH = os.path.abspath(os.path.join(__file__, '..', '..', '..', 'static', 'icons', self.NAME_ICON))
         return ctk.CTkImage(
             light_image= Image.open(PATH),
-            size= (self.MASTER._current_height * 0.7, self.MASTER._current_height * 0.7)
+            size= self.SIZE
         )
 
